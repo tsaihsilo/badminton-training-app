1200241e4861332497032cda7f494f5a8b1a4f30
Selena Lo <tsaihsilo@gmail.com>
2025-10-27 22:52:50 +0800

    refactor(backend): use AUTH_USER_MODEL in UserProfile

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class UserProfile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
   is_instructor = models.BooleanField(default=False)
 
   def __str__(self):
