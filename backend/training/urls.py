1592967214202467c584dfdea41f3cf9d542f9ae
Selena Lo <tsaihsilo@gmail.com>
2025-11-06 16:21:39 +0800

    feat(backend): add enrollment and student API routes

--- backend/training/urls.py
+++ backend/training/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import DrillListView
+from .views import DrillListView, EnrollmentListCreateView, EnrollmentDetailView, StudentListView
 
 urlpatterns = [
   path('drills/', DrillListView.as_view(), name='drill-list'),
+  path('enrollments/', EnrollmentListCreateView.as_view(), name="enrollment-list-create"),
+  path('enrollment/<int:pk>/', EnrollmentDetailView.as_view(), name="enrollment-detail"),
+  path('students/', StudentListView.as_view(), name="student-list"),
 ]
