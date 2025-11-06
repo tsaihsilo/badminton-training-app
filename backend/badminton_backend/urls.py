fe9a4ab0cf4807ae870ad6480467289b08839e52
Selena Lo <tsaihsilo@gmail.com>
2025-11-06 15:46:49 +0800

    chore(backend): add DRF auth UI for dev testing

--- backend/badminton_backend/urls.py
+++ backend/badminton_backend/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('_allauth/', include('allauth.headless.urls')),
+  path("api-auth/", include("rest_framework.urls")),
   path('api/training/', include('training.urls')),
 ]
