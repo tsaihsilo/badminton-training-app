5129852c2eac528e56dd4691aa63a7521bb774a1
Selena Lo <tsaihsilo@gmail.com>
2025-11-06 16:19:21 +0800

    feat(backend): add EnrollmentSerializer and StudentSerializer

--- backend/training/serializers.py
+++ backend/training/serializers.py
@@ -1,7 +1,28 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Drill
+from .models import Drill, Enrollment
+
+User = get_user_model()
 
 class DrillSerializer(serializers.ModelSerializer):
   class Meta:
     model = Drill
-    fields = ['id', 'title', 'video_url', 'is_active']
+    fields = ["id", "title", "video_url", "is_active"]
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+  instructor_username = serializers.CharField(source="instructor.username", read_only=True)
+  student_username = serializers.CharField(source="student.username", read_only=True)
+
+  class Meta:
+    model = Enrollment
+    fields = ["id", "instructor", "student", "instructor_username", "student_username"]
+    extra_kwargs = {
+      "instructor": {"read_only": True}
+    }
+
+
+class StudentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ["id", "username"]
