0d4fb337d5ac1f64ea0f20e2ae9f316c747aa27d
Selena Lo <tsaihsilo@gmail.com>
2025-11-09 23:17:57 +0800

    feat(backend): add IsStudent permission class

--- backend/training/permissions.py
+++ backend/training/permissions.py
@@ -4,3 +4,8 @@ class IsInstructor(permissions.BasePermission):
   """Allow access only to instructor users."""
   def has_permission(self, request, view):
     return request.user.userprofile.is_instructor
+  
+class IsStudent(permissions.BasePermission):
+  """Allow access only to student users."""
+  def has_permission(self, request, view):
+    return not request.user.userprofile.is_instructor
