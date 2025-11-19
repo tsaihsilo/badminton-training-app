ea64a89d116df929406bf3a2c915435ad97e0eec
Selena Lo <tsaihsilo@gmail.com>
2025-11-19 23:11:14 +0800

    test(backend): add TrainingModelTests for drill/enrollment/assignment

--- backend/training/tests.py
+++ backend/training/tests.py
@@ -1,3 +1,23 @@
+from django.db import IntegrityError
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Drill, Enrollment, Assignment
 
-# Create your tests here.
+User = get_user_model()
+
+class TrainingModelTests(TestCase):
+  def setUp(self):
+    self.instructor = User.objects.create_user(username="kevin", password="20021031")
+    self.student = User.objects.create_user(username="selena", password="20040303")
+    self.drill = Drill.objects.create(title="Footwork", video_url="https://footwork/example")
+    self.enrollment = Enrollment.objects.create(instructor=self.instructor, student=self.student)
+
+  def test_enrollment_str(self):
+    text = str(self.enrollment)
+    self.assertIn("selena", text)
+    self.assertIn("kevin", text)
+
+  def test_assignment_unique_constraint(self):
+    Assignment.objects.create(enrollment=self.enrollment, drill=self.drill)
+    with self.assertRaises(IntegrityError):
+      Assignment.objects.create(enrollment=self.enrollment, drill=self.drill)
