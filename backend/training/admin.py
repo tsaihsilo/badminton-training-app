9b0784029f12454ae3d4895e1bb191d73a76d5b6
Selena Lo <tsaihsilo@gmail.com>
2025-11-22 18:34:55 +0800

    chore(backend): register Assignment model in admin

--- backend/training/admin.py
+++ backend/training/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Drill, Enrollment
+from .models import Drill, Enrollment, Assignment
 
 admin.site.register(Drill)
 admin.site.register(Enrollment)
+admin.site.register(Assignment)
