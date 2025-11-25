edf9aa6e9ff1c1b60d64f0961c401b14e8127357
Selena Lo <tsaihsilo@gmail.com>
2025-11-25 17:24:50 +0800

    feat(backend): add UserProfile model to accounts app

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class UserProfile(models.Model):
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+  is_instructor = models.BooleanField(default=False)
+
+  def __str__(self):
+    role = "Instructor" if self.is_instructor else "Student"
+    return f"{self.user.username} ({role})"
