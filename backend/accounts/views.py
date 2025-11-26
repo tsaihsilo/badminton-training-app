2dba872605cd6fecf0a44a7a134c72868ea8351d
Selena Lo <tsaihsilo@gmail.com>
2025-11-26 15:53:41 +0800

    feat(backend): add SignupView endpoint for user signup

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializer
 
-# Create your views here.
+class SignupView(APIView):
+  
+  def post(self, request):
+    serializer = SignupSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
