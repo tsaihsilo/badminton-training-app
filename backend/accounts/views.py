2c1244a2b1915500c8c4e33265041f8433d0720e
Selena Lo <tsaihsilo@gmail.com>
2025-11-26 17:59:26 +0800

    feat(backend): add token generation to signup endpoint

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,12 +1,20 @@
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignupSerializer
+from rest_framework.authtoken.models import Token
 
 class SignupView(APIView):
-  
+  permission_classes = []
+  authentication_classes = []
+
   def post(self, request):
     serializer = SignupSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
+    user = serializer.save()
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response(
+      {"token": token.key}, 
+      status=status.HTTP_201_CREATED,
+    )
