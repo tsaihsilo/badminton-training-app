7d3aa21612a8b9004f1ba8352005762a5f70694a
Selena Lo <tsaihsilo@gmail.com>
2025-12-03 12:03:56 +0800

    feat(frontend): create useDrills hook to fetch and store drill data

---
+++ frontend/src/features/instructor/hooks/useDrills.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query"
+import { getDrills } from "../api/getDrills"
+
+export const useDrills = () => {
+  return useQuery({
+    queryKey: ["drills"],
+    queryFn: getDrills,
+  })
+}
