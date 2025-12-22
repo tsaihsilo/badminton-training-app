4122f50b06ed773219c5ab4fbe43a08b75328fc0
Selena Lo <tsaihsilo@gmail.com>
2025-12-22 16:16:59 -0500

    fix(frontend): add fallback if there are no students enrolled

--- frontend/src/features/instructor/AssignDrillsPage.tsx
+++ frontend/src/features/instructor/AssignDrillsPage.tsx
@@ -4,6 +4,7 @@ import { useDrills } from "./hooks/drills/useDrills";
 import { useEnrollments } from "./hooks/enrollments/useEnrollments";
 import { Enrollment } from "./ManageStudentsPage";
 import { useAssignments } from "./hooks/assignments/useAssignments";
+import { colors } from "../../shared/constants";
 
 export const AssignDrillsPage = () => {
   const { data: enrollments = [] } = useEnrollments();
@@ -12,15 +13,21 @@ export const AssignDrillsPage = () => {
 
   return (
     <Box sx={{ padding: "60px 0"}}>
-      {enrollments.map((enroll: Enrollment) => (
-        <AssignDrillRow 
-          key={enroll.id}
-          enrollmentId={enroll.id}
-          studentName={enroll.student_username}
-          assignments={assignments}
-          drills={drills}
-        />
-      ))}
+      {enrollments.length === 0 ? (
+        <Box sx={{ px: 4, color: colors.inPageText, mt: 4, ml: 7 }}>
+          No students enrolled yet.
+        </Box>
+      ) : (
+        enrollments.map((enroll: Enrollment) => (
+          <AssignDrillRow 
+            key={enroll.id}
+            enrollmentId={enroll.id}
+            studentName={enroll.student_username}
+            assignments={assignments}
+            drills={drills}
+          />
+        ))
+      )}
     </Box>
   )
 }
